37e8b489c6024bae362a4fca0eb72afbc26c1a81
ayhan7373 <aseman.ayhan@gmail.com>
2024-10-12 18:46:37 +0330

    changed files name

--- runner_bash.sh
+++ runner_bash.sh
@@ -12,12 +12,12 @@ read -p "Enter a number: " number
 
 SESSION_NAME="map"
 
-python3 ./cord.py > output.txt
+python3 ./Polygonal_conversion_to_kml_files_based_on_latitude.py > output.txt
 
 sleep 5
 
 cp output/lat_$number/* .
 
-tmux new-session -d -s $SESSION_NAME 'python3 ./map.py; exec bash'
+tmux new-session -d -s $SESSION_NAME 'python3 ./Download_map_using_kml_file_compress_and_send_using_ftp.py; exec bash'
 
 echo "Started new tmux session: $SESSION_NAME"
